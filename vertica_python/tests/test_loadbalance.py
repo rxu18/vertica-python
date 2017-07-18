8324cec50e9000a434e5299d2688e64c63ec452f
tumuluru <udit.tumuluri@hpe.com>
2017-07-18 13:20:51 -0400

    Connection logic: -Moved socket creation logic while trying to connect. If connection fails, close old socket and create new one -Changed variable name for exception since there is another variable in the context manager with same name therefore reducing the scope of the older variable. Also initializing the variable before while loop to avoid exception in case the loop is not entered
    Tests:
    -Added setUp and tearDown for test cases to restore changed connection details(host, port)
    -Additional Test case that validates exception handling when there is no host available to connect
    -Added test case that validates exception handling when no hosts and ports are provided by user
    -Added test case that validates exception handling when hosts and ports given by user are not equal in number

--- vertica_python/tests/test_loadbalance.py
+++ vertica_python/tests/test_loadbalance.py
@@ -1,6 +1,15 @@
-from base import VerticaPythonTestCase
+from .base import VerticaPythonTestCase
+from .. import errors
 
 class TestLoadBalance(VerticaPythonTestCase):
+    def setUp(self):
+        self.host = self._conn_info['host']
+        self.port = self._conn_info['port']
+
+    def tearDown(self):
+        self._conn_info['host'] = self.host
+        self._conn_info['port'] = self.port
+
     def test_loadbalance_option_not_set(self):
         with self._connect() as conn:
             self.assertTrue(conn.socket is not None)
@@ -30,3 +39,27 @@ class TestLoadBalance(VerticaPythonTestCase):
             self.assertTrue(conn.socket is not None)
             self.assertTrue(not conn.socket._closed)
             pass
+
+    def test_loadbalance_failover_both_host_port_invalid(self):
+        self._conn_info['host'] = ['invalid', 'invalid']
+        port = self._conn_info['port']
+        self._conn_info['port'] = [port, port]
+
+        with self.assertRaises(errors.ConnectionError):
+            with self._connect():
+                pass
+
+    def test_loadbalance_when_no_host_port_are_provided(self):
+        self._conn_info['host'] = []
+        self._conn_info['port'] = []
+        with self.assertRaises(errors.ConnectionError):
+            with self._connect():
+                pass
+
+    def tes_loadbalance_when_host_port_length_doesnt_match(self):
+        self._conn_info['host'] = ['engdev3']
+        port = self._conn_info['port']
+        self._conn_info['port'] = [port, port]
+        with self.assertRaises(errors.ConnectionError):
+            with self._connect():
+                pass
