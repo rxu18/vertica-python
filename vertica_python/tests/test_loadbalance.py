d080bca716b8d803d0c3a7476eb686036b36fe5f
tumuluru <udit.tumuluri@hpe.com>
2017-07-18 18:44:36 -0400

    Test Cases fix after running test cases on python 2.7 and python 3.6 using tox tool: -Replaced hardcoded host and port details and fetched from the inital setUp -setUp within tests is redundant, host and port details are stored in base test class setUp -moved socket _close field check as this is not available in python 2.7

--- vertica_python/tests/test_loadbalance.py
+++ vertica_python/tests/test_loadbalance.py
@@ -2,42 +2,34 @@ from .base import VerticaPythonTestCase
 from .. import errors
 
 class TestLoadBalance(VerticaPythonTestCase):
-    def setUp(self):
-        self.host = self._conn_info['host']
-        self.port = self._conn_info['port']
-
     def tearDown(self):
-        self._conn_info['host'] = self.host
-        self._conn_info['port'] = self.port
+        self._conn_info['host'] = self._host
+        self._conn_info['port'] = self._port
 
     def test_loadbalance_option_not_set(self):
         with self._connect() as conn:
             self.assertTrue(conn.socket is not None)
-            self.assertTrue(not conn.socket._closed)
             pass
 
     def test_loadbalance_true(self):
         self._conn_info['load'] = True
         with self._connect() as conn:
             self.assertTrue(conn.socket is not None)
-            self.assertTrue(not conn.socket._closed)
             pass
 
     def test_loadbalance_false(self):
         self._conn_info['load'] = False
         with self._connect() as conn:
             self.assertTrue(conn.socket is not None)
-            self.assertTrue(not conn.socket._closed)
             pass
 
     def test_loadbalance_failover_first_host_port_invalid(self):
-        self._conn_info['host'] = ['invalid', 'engdev3']
+        self._conn_info['host'] = ['invalid', self._host]
         port = self._conn_info['port']
         self._conn_info['port'] = [port, port]
 
         with self._connect() as conn:
             self.assertTrue(conn.socket is not None)
-            self.assertTrue(not conn.socket._closed)
             pass
 
     def test_loadbalance_failover_both_host_port_invalid(self):
@@ -56,10 +48,9 @@ class TestLoadBalance(VerticaPythonTestCase):
             with self._connect():
                 pass
 
-    def tes_loadbalance_when_host_port_length_doesnt_match(self):
-        self._conn_info['host'] = ['engdev3']
-        port = self._conn_info['port']
-        self._conn_info['port'] = [port, port]
+    def test_loadbalance_when_host_port_length_doesnt_match(self):
+        self._conn_info['host'] = [self._host]
+        self._conn_info['port'] = [self._port, self._port]
         with self.assertRaises(errors.ConnectionError):
             with self._connect():
                 pass
