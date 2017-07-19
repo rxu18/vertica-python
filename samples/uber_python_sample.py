03aa684933d93e7247526e03ce8695aae59c88b7
tudit <udittumuluri@gmail.com>
2017-07-19 12:25:11 -0400

    Checkin correction: Removing local files

--- samples/uber_python_sample.py
+++
@@ -1,9 +0,0 @@
-from vertica_python_project import vertica_python
-from connection_info import CONN_INFO
-
-#sample to connect and run vertica queries
-with vertica_python.connect(**CONN_INFO) as conn:
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM hello_world')
-    for row in cursor.iterate():
-        print(row)
