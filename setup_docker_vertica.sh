e2e9168129b48c07ed21ffcfd47388b178731fdc
Richard Xu <richard.xu@microfocus.com>
2019-07-31 10:15:16 -0400

    Improve option-passing in docker script and add --help.

--- setup_docker_vertica.sh
+++ setup_docker_vertica.sh
@@ -4,8 +4,8 @@ install_kdc(){
 	echo "Setting up KDC"
 	docker network create test
 	docker build -t vertica/kdc docker/kdc
-	docker run -d --privileged --name=vp.kdc --network=test vertica/kdc
-	docker exec vp.kdc /kdc/install_kdc.sh
+	docker run -d --privileged --name=$VP_KDC_NAME --network=test vertica/kdc
+	docker exec $VP_KDC_NAME /kdc/install_kdc.sh
 }
 
 create_db(){
@@ -16,24 +16,24 @@ create_db(){
 		curl $VERTICA_CE_URL --create-dirs -o docker-vertica/packages/vertica-ce.latest.rpm
 	fi
 	docker build -f docker-vertica/Dockerfile.centos.7_9.x --build-arg VERTICA_PACKAGE=vertica-ce.latest.rpm -t jbfavre/vertica docker-vertica
-	docker run -d -p 5433:5433 --name=vp.db --network=test jbfavre/vertica
+	docker run -d -p 5433:5433 --name=$VP_DB_NAME --network=test jbfavre/vertica
 	
 	echo "Making service keytab"
 	export V_PRINC=vertica/vertica.example.com@EXAMPLE.COM
-	docker exec vp.kdc kadmin.local -q "addprinc -randkey ${V_PRINC}"
-	docker exec vp.kdc kadmin.local -q "ktadd -norandkey -k vertica.keytab ${V_PRINC}"
-	docker cp vp.kdc:vertica.keytab .
-	docker cp vertica.keytab vp.db:/
+	docker exec $VP_KDC_NAME kadmin.local -q "addprinc -randkey ${V_PRINC}"
+	docker exec $VP_KDC_NAME kadmin.local -q "ktadd -norandkey -k vertica.keytab ${V_PRINC}"
+	docker cp $VP_KDC_NAME:vertica.keytab .
+	docker cp vertica.keytab $VP_DB_NAME:/
 	rm vertica.keytab
 
 	echo "Waiting for db to start"
 	sleep 60
 
 	echo "Kerberize db"
-	docker cp docker/kdc/kerberize.sh vp.db:/
-	docker exec vp.db yum install -y krb5-workstation
-	docker exec vp.db /bin/sh -c "echo $(docker inspect -f '{{.NetworkSettings.Networks.test.IPAddress }}' vp.kdc) kerberos.example.com >> /etc/hosts"
-	docker exec vp.db /kerberize.sh
+	docker cp docker/kdc/kerberize.sh $VP_DB_NAME:/
+	docker exec $VP_DB_NAME yum install -y krb5-workstation
+	docker exec $VP_DB_NAME /bin/sh -c "echo $(docker inspect -f '{{.NetworkSettings.Networks.test.IPAddress }}' $VP_KDC_NAME) kerberos.example.com >> /etc/hosts"
+	docker exec $VP_DB_NAME /kerberize.sh
 }
 
 test_python(){
@@ -45,27 +45,68 @@ test_python(){
 
 stop_container(){	
 	echo "Stopping containers"
-	docker container stop vp.kdc
-	docker container stop vp.db
+	docker container stop $VP_KDC_NAME
+	docker container stop $VP_DB_NAME
 }
 
 clean_system(){
 	echo "Cleaning up"
-	docker container rm vp.kdc
-	docker container rm vp.db
+	docker container rm $VP_DB_NAME
+	docker container rm $VP_DB_NAME
 	docker image rm jbfavre/vertica
 	docker image rm vertica/kdc
 	docker network remove test
 }
 
 echo_use(){
-	echo "Usage: $0 [start|test|stop|clean]"
+	echo "Usage: $0 [arguments] command"
 }
-op=$1
+
+echo_help(){
+	echo "$0 sets up a kerberos-enabled vertica database to facilitate testing."
+	echo "Usage: $0 [arguments] command"
+	echo
+	echo "Commands: [start|test|stop|clean]"
+	echo "Start builds vertica and kerberos."
+	echo "Test builds and runs the vertica-python test suite."
+	echo "Stop stops the containers."
+	echo "Clean removes the containers and the images."
+	echo
+	echo "Options: --kdc, --db, --py"
+	echo "--kdc specifies the name of the kdc container (default: vp.kdc)"
+	echo "--db specifies the name of the database container (default: vp.db)"
+	echo "--py specifies the version of python. Options: py37 (default),py27,py34,py35,py36."
+}
 
 if [ $# -eq 0 ]; then
 	echo_use
-elif [ $1 = 'start' ]; then
+	return 0
+fi
+
+VP_KDC_NAME='vp.kdc'
+VP_DB_NAME='vp.db'
+while [ -n "$1" ]; do
+	case "$1" in
+	--help) echo_help
+	exit 0
+	;;
+	--kdc) VP_KDC_NAME=$2
+	shift
+	;;
+	--db) VP_DB_NAME=$2
+	shift
+	;;
+	--py) PYENV=$2
+	;;	
+	--) shift
+	break ;;
+	*) break ;;
+	esac	
+	shift
+done
+
+op=$1
+if [ op = 'start' ]; then
 	install_kdc
 	create_db
 elif [ $1 = 'test' ]; then
